644d2701a514af478202e80ed45899620c65d314
Dorr-hlc <1726660621@qq.com>
2023-02-06 17:24:47 +0800

    静态

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+/*
+ * @Copyright: Copyright© 2022 AOMEI
+ * @Abstract: 
+ * @Date: 2023-02-06 10:24:08
+ * @Author: 
+ * @LastEditors: houliucun
+ * @LastEditTime: 2023-02-06 10:40:27
+ * @RevisionHistory: 
+ */
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  lintOnSave:false, //关闭语法检查
+})
